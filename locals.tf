08709cc35062e88b3159c3506ac594625fd5d967
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 15:48:00 +0530

    Add files via upload

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals{
+    # OCI constants
+    availability_domain         = "1"
+    network_cidrs = {
+        vcn_cidr                     = "10.0.0.0/16"
+        load_balancer_subnet_cidr    = "10.0.20.0/24"
+        
+    }
+}
