12a0087aaea1d89f4a38428c6fbc1283b3fa3c3a
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 15:41:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,47 @@
-// --- configuration --- //
-module "compute" {
-  source                    = "github.com/avaloqcloud/acf_resource_compute"
-  compartment_id            = var.compartment_ocid
-  availability_domain       = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name")}"
-  public_subnet_id          = oci_core_subnet.load_balancer_subnet.id
-  ssh_authorized_key        = tls_private_key.ssh_key.public_key_openssh
-  ssh_private_key           = tls_private_key.ssh_key.private_key_pem
-  oci_cli_api_public_key    = tls_private_key.ssh_key.public_key_openssh
-  oci_cli_api_private_key   = tls_private_key.ssh_key.private_key_pem
-  user_id                   = var.current_user_ocid
-  tenancy_id                = data.oci_identity_user.user.compartment_id
-  depends_on                = [module.acf_resource_network]
+resource "oci_core_instance" "compute_instance" {
+  availability_domain = var.availability_domain
+  compartment_id = var.compartment_id
+  display_name = var.vm_name
+  shape = var.shape_id
+
+  shape_config {
+    memory_in_gbs = var.memory
+    ocpus = var.ocpus
+  }
+ 
+  create_vnic_details {
+    subnet_id = var.public_subnet_id
+    display_name = "compute_instance"
+    assign_public_ip = true
+    #hostname_label = "compute_instance"
+  }
+ 
+  source_details {
+    source_type = "image"
+    source_id = var.image_id
+    boot_volume_size_in_gbs = "50"
+  } 
+  
+  metadata = {
+    ssh_authorized_keys = var.ssh_authorized_key
+  
+  }
+  timeouts {
+    create = "60m"
+  }
+}
+
+resource "null_resource" "copy_private_key" {
+  provisioner "file" {
+    connection {
+      agent       = false
+      timeout     = "30m"
+      host        = oci_core_instance.compute_instance.public_ip
+      user        = "opc"
+      private_key = var.ssh_private_key
+    }
+  
+    content = var.ssh_private_key
+    destination = "~/.ssh/id_rsa.pem"
+  } 
 }
