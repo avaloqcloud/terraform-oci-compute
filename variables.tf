b242dac0772e0ca1ca576a6af9bc54d0fd3d9e3b
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 17:37:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable "compartment_id" {
+variable "compartment_ocid" {
   type =        string
   description = "Compartment OCID"
 }
 
-variable "availability_domain" {
-  type =        string
-  description = "Availability Domain"
-}
+#variable "availability_domain" {
+#  type =        string
+#  description = "Availability Domain"
+#}
 
-variable "public_subnet_id" {
-  type =        string
-  description = "OCID of public subnet in which to create a compute_instance host"
-}
+#variable "public_subnet_id" {
+#  type =        string
+#  description = "OCID of public subnet in which to create a compute_instance host"
+#}
 
 variable "shape_id" {
   type =        string
@@ -38,15 +38,15 @@ variable "vm_name" {
   description = "VM Name"
 }
 
-variable "ssh_authorized_key" {
-  type =        string
-  description = "RSA public key in PEM format. It will added as authorized key for remote connection over SSH"
-}
+#variable "ssh_authorized_key" {
+#  type =        string
+#  description = "RSA public key in PEM format. It will added as authorized key for remote connection over SSH"
+#}
 
-variable "ssh_private_key" {
-  type =        string
-  description = "SSH Private key in PEM format. The private key will be used for connecting to the VMs (compute_instance, ACP, OKE nodes) over SSH"
-}
+#variable "ssh_private_key" {
+#  type =        string
+#  description = "SSH Private key in PEM format. The private key will be used for connecting to the VMs (compute_instance, ACP, OKE nodes) over SSH"
+#}
 
 variable "oci_cli_api_public_key" {
   type =        string
