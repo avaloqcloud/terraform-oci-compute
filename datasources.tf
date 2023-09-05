72eec5fb1310c8013fd87cf86d864bd2c6a11bb3
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 17:51:16 +0530

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,6 +1,6 @@
 data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.compartment_ocid
 }
-data "oci_identity_user" "user" {
-    user_id  = var.current_user_ocid
-}
+#data "oci_identity_user" "user" {
+#    user_id  = var.current_user_ocid
+#}
