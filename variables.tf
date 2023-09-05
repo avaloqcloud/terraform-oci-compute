33813d401fb60eaaa96604f149d423f4247f522b
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 16:37:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+variable "availability_domain" {
+  type =        string
+  description = "Availability Domain"
+}
+
+variable "public_subnet_id" {
+  type =        string
+  description = "OCID of public subnet in which to create a Bastion host"
+}
+
+variable "ssh_authorized_key" {
+  type =        string
+  description = "RSA public key in PEM format. It will added as authorized key for remote connection over SSH"
+}
+
+variable "compartment_id" {
+   description = "The compartment Id within the OCI (Oracle Cloud Infrastructure) tenancy"
+}
+
 variable "compartment_ocid" {
    description = "The compartment Id within the OCI (Oracle Cloud Infrastructure) tenancy"
 }
