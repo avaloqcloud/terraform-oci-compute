dfc77d3f4a60a239b903f5f0df4ae3e323a81fcb
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-05 16:51:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "oci_core_instance" "compute_instance" {
   availability_domain = var.availability_domain
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_ocid
   display_name = var.vm_name
   shape = var.shape_id
 
@@ -49,7 +49,7 @@ resource "null_resource" "copy_private_key" {
 resource "oci_core_vcn" "compute_vcn" {
   #Required
   cidr_block     = "${lookup(local.network_cidrs, "vcn_cidr")}"
-  compartment_id = var.compartment_id
+  compartment_id = var.compartment_ocid
 
 }
 
@@ -76,7 +76,7 @@ resource "oci_core_security_list" "worker_node_sl" {
 resource "oci_core_subnet" "load_balancer_subnet" {
   #Required
   cidr_block        = "${lookup(local.network_cidrs, "load_balancer_subnet_cidr")}"
-  compartment_id    = var.compartment_id
+  compartment_id    = var.compartment_ocid
   security_list_ids = ["${oci_core_security_list.worker_node_sl.id}"]
   vcn_id            = "${oci_core_vcn.compute_vcn.id}"
 
