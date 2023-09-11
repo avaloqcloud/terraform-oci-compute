630fa236261221f747c8b2ec28588238fbba18ac
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-09-11 11:35:01 +0530

    Removed network defination, added metadata configuration

--- main.tf
+++ main.tf
@@ -1,107 +1,38 @@
+provider "oci" {
+  region = "${var.region}"
+}
 
 resource "oci_core_instance" "compute_instance" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name")}"
-  #availability_domain = var.availability_domain
+  #availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name")}"
+  availability_domain = var.availabilitydomain
   compartment_id = var.compartment_ocid
   display_name = var.vm_name
-  shape = var.shape_id
+  shape = var.instance_shape
+
+  freeform_tags  = {"instance-name"="${var.vm_name}"}
+  #defined_tags   = {"instance-name"="${var.vm_name}"}
 
   shape_config {
     memory_in_gbs = var.memory
     ocpus = var.ocpus
   }
  
-  create_vnic_details {
-    subnet_id = oci_core_subnet.load_balancer_subnet.id
-    #subnet_id = var.public_subnet_id
-    display_name = "compute_instance"
-    assign_public_ip = true
-    #hostname_label = "compute_instance"
-  }
- 
   source_details {
     source_type = "image"
-    source_id = var.image_id
+    source_id = var.instance_image
     boot_volume_size_in_gbs = "50"
   } 
   
   metadata = {
-    ssh_authorized_keys = tls_private_key.ssh_key.public_key_openssh
-    #ssh_authorized_keys = var.ssh_authorized_key
+    ssh_authorized_keys = var.ssh_public_key
+    #ssh_authorized_keys = tls_private_key.ssh_key.public_key_openssh
+    user_data = var.cloud_init_script
   
   }
   timeouts {
     create = "60m"
   }
 }
-
-resource "null_resource" "copy_private_key" {
-  provisioner "file" {
-    connection {
-      agent       = false
-      timeout     = "30m"
-      host        = oci_core_instance.compute_instance.public_ip
-      user        = "opc"
-      private_key = tls_private_key.ssh_key.private_key_pem
-      #private_key = var.ssh_private_key
-    }
-  
-    content = tls_private_key.ssh_key.private_key_pem
-    destination = "~/.ssh/id_rsa.pem"
-  } 
-}
-
-resource "oci_core_vcn" "compute_vcn" {
-  #Required
-  cidr_block     = "${lookup(local.network_cidrs, "vcn_cidr")}"
-  compartment_id = "${var.compartment_ocid}"
-
-}
-
-# https://www.terraform.io/docs/providers/oci/r/core_security_list.html
-resource "oci_core_security_list" "worker_node_sl" {
-  #Required
-  compartment_id = "${var.compartment_ocid}"
-  vcn_id         = "${oci_core_vcn.compute_vcn.id}"
-
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol = "all" 
-  }
-
-  ingress_security_rules {
-    protocol = "all" 
-    source = "0.0.0.0/0"
-  }
-  #Optional
-  display_name = "worker node security list"
-}
-
-
-resource "oci_core_subnet" "load_balancer_subnet" {
-  #Required
-  cidr_block        = "${lookup(local.network_cidrs, "load_balancer_subnet_cidr")}"
-  compartment_id    = "${var.compartment_ocid}"
-  security_list_ids = ["${oci_core_security_list.worker_node_sl.id}"]
-  vcn_id            = "${oci_core_vcn.compute_vcn.id}"
-
-  #Optional
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name")}"
-  dhcp_options_id     = "${oci_core_vcn.compute_vcn.default_dhcp_options_id}"
-  display_name        = "load balancer subnet"
-  #dns_label           = "lb"
-}
-
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-#resource "oci_identity_api_key" "user_api_key" {
-#    #Required
-#    key_value = tls_private_key.ssh_key.public_key_pem
-#    user_id   = var.current_user_ocid
-#}
 
 output "instance_private_ip" {
   value = oci_core_instance.compute_instance.private_ip
