6b9a624900903d312cad6c4c8d9a2a69704db7b1
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-11-08 15:18:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
-provider "oci" {
-  region = "${var.region}"
+terraform {
+    required_version = "~> 1.2"
+    required_providers {
+        oci = {
+            source  = "oracle/oci"
+            version = "5.18.0"
+        }
+    }
 }
