cf84e5653bd787ec99d3a196716881b6aa9aed46
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-11-08 15:25:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
-    required_version = "~> 1.2"
-    required_providers {
-        oci = {
-            source  = "oracle/oci"
-            version = "5.18.0"
-        }
+  required_version = "~> 1.2"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.39"
     }
+    oci = {
+      source  = "oracle/oci"
+      version = "5.18.0"
+    }
+  }
 }
