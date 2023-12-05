b1cf004df4c762b0066bab4222be219d6f331402
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-12-05 13:49:02 +0530

    new-code-approach

--- main.tf
+++ main.tf
@@ -1,29 +1,41 @@
-resource "oci_core_instance" "compute_instance" {
-  availability_domain = var.availabilitydomain
-  compartment_id = var.compartment_ocid
-  display_name = var.vm_name
-  shape = var.instance_shape
+resource "oci_artifacts_container_repository" "this" {
+  for_each = local.potential_registry
+  # Required
+  compartment_id = each.value.compartment_id
+  display_name   = each.value.display_name
+  # Optional
+  defined_tags  = each.value.defined_tags
+  freeform_tags = each.value.freeform_tags
+}
+
+
+resource "oci_core_instance" "this" {
+  for_each = local.potential_instance
+  availability_domain = each.value.availabilitydomain
+  compartment_id = each.value.compartment_ocid
+  display_name = each.value.vm_name
+  shape = each.value.instance_shape
 
   create_vnic_details {
-        subnet_id = var.subnet_id
-        assign_public_ip = var.assign_public_ip
+        subnet_id = each.value.subnet_id
+        assign_public_ip = each.value..assign_public_ip
     }
 
 
   shape_config {
-    memory_in_gbs = var.memory
-    ocpus = var.ocpus
+    memory_in_gbs = each.value..memory
+    ocpus = each.value.ocpus
   }
  
   source_details {
     source_type = "image"
-    source_id = var.instance_image
-    boot_volume_size_in_gbs = var.boot_volume_size_in_gbs
+    source_id = each.value.instance_image
+    boot_volume_size_in_gbs = each.value.boot_volume_size_in_gbs
   } 
   
   metadata = {
-    ssh_authorized_keys = var.ssh_public_key
-    user_data = base64encode(var.cloud_init_script)
+    ssh_authorized_keys = each.value.ssh_public_key
+    user_data = base64encode(each.value.cloud_init_script)
 
   }
   timeouts {
@@ -31,11 +43,11 @@ resource "oci_core_instance" "compute_instance" {
   }
 }
 
-output "instance_private_ip" {
-  value = oci_core_instance.compute_instance.private_ip
-  description = "Private ip of the instance"
-}
-output "compute_instance_id" {
-  value = oci_core_instance.compute_instance.id
-  description = "ID of the instance"
-}
+#output "instance_private_ip" {
+#  value = oci_core_instance.compute_instance.private_ip
+#  description = "Private ip of the instance"
+#}
+#output "compute_instance_id" {
+#  value = oci_core_instance.compute_instance.id
+#  description = "ID of the instance"
+#}
