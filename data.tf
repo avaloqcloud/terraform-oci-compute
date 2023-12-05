567cdc0b8cec24fb04121da4954ea94c7c85feec
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-12-05 16:52:25 +0530

    new code approach

---
+++ data.tf
@@ -0,0 +1,15 @@
+# Data lookups of existing resources
+## Compute Instance
+data "oci_core_instance" "existing_instance" {
+    #Required
+    instance_id = var.instance.id      # var.oci_core_instance.test_instance.id
+}
+  # Filter - Important; Filter should be so that only ever one gets returned.
+  filter {
+    name   = "vm_name"
+    values = ["${var.instance.vm_name}"]
+  }
+}
+
+
+
