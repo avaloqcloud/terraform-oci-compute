4962e348f726b0fd776bf753250a76fdb3df3f82
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-12-05 16:04:24 +0530

    new code approach

--- variables.tf
+++ variables.tf
@@ -1,62 +1,18 @@
-variable "assign_public_ip" {
-  type =        string
-  description = "Assign public IP to the instance"
-  default = false
+variable "instance" {
+  description = "Compute instance input object"
+  type = object({
+    assign_public_ip = string,
+    boot_volume_size_in_gbs = string, 
+    compartment_ocid = string,
+    cloud_init_script = string,
+    subnet_id = string,
+    ssh_public_key = string,
+    instance_shape = string,
+    instance_image = string,
+    memory = string,
+    ocpus = string,
+    vm_name = string,
+    availabilitydomain = string
+  })
 }
 
-variable "boot_volume_size_in_gbs" {
-  type =        string
-  description = "Compartment OCID"
-  default = "500"
-}
-
-variable "compartment_ocid" {
-  type =        string
-  description = "Compartment OCID"
-}
-
-variable "cloud_init_script" {
-  type = string
-  description = "Cloud init script"
-}
-
- variable "subnet_id" {
-  type = string
-  description = "Subnet ID"
-   
- }
-
-variable "ssh_public_key" {
-  type = string
-  description = "SSH public key"
-}
-
-variable "instance_shape" {
-  type =        string
-  description = "Image Shape"
-}
-
-variable "instance_image" {
-  type =        string
-  description = "Image ID"
-}
-
-variable "memory" {
-  type =        string
-  description = "Memory size"
-}
-
-variable "ocpus" {
-  type =        string
-  description = "OCPU Size"
-}
-
-variable "vm_name" {
-  type =        string
-  description = "VM Name"
-}
-
- variable "availabilitydomain" {
-   type = string
-   description = "Availability Domain"
- }
