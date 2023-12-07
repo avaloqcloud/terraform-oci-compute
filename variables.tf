4a4afb7e9516f02a764f0bc09ae29dcc92e013d7
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-12-07 15:16:40 +0530

    New code approach

--- variables.tf
+++ variables.tf
@@ -2,15 +2,15 @@ variable "instance" {
   description = "Compute instance input object"
   type = object({
     assign_public_ip = string, #default = false
-    boot_volume_size_in_gbs = string, #default = "500"
+    boot_volume_size_in_gbs = number, #default = "500"
     compartment_ocid = string,
     cloud_init_script = string,
     subnet_id = string,
     ssh_public_key = string,
     instance_shape = string,
     instance_image = string,
-    memory = string,
-    ocpus = string,
+    memory = number,
+    ocpus = number,
     vm_name = string,
     availabilitydomain = string
   })
