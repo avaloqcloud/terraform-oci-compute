034b6bdc4071098c6cb71a0d94fd0edc854b96d1
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2023-12-11 11:01:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "oci_artifacts_container_repository" "this" {
-  for_each = local.potential_registry
-  # Required
-  compartment_id = each.value.compartment_id
-  display_name   = each.value.display_name
-  # Optional
-  defined_tags  = each.value.defined_tags
-  freeform_tags = each.value.freeform_tags
-}
-
-
 resource "oci_core_instance" "this" {
   for_each = local.potential_instance
   availability_domain = each.value.availabilitydomain
