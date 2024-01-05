13e81e7dfa4782ce442b54d33e94d20b86800c4d
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2024-01-05 16:27:06 +0530

    Plugins disabled false

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "oci_core_instance" "compute_instance" {
 
   agent_config {
     are_all_plugins_disabled = false
-    is_management_disabled = true
-    is_monitoring_disabled = true
+    is_management_disabled = false
+    is_monitoring_disabled = false
     plugins_config {
         name = "Bastion"
         desired_state = var.bastion_desired_state
