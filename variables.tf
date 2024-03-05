7bee61c9df6f63c76c78b653f0c76a7b6dfeaaac
AvinashBodhe1 <135799650+AvinashBodhe1@users.noreply.github.com>
2024-03-05 14:37:23 +0530

    Add static private IP to the instance (#11)
    * private IP update
    
    * private ip default vakue

--- variables.tf
+++ variables.tf
@@ -26,6 +26,12 @@ variable "cloud_init_script" {
    
  }
 
+variable "private_ip" {
+  type = string
+  description = "A private IP address of your choice to assign to the VNIC. Must be an available IP address within the subnet's CIDR."
+  default = ""
+}
+
 variable "ssh_public_key" {
   type = string
   description = "SSH public key"
