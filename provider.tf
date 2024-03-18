4ae8de205a24e8bd17d22cfa02aff984e74ae01b
Avinash Bodhe <avinash.bodhe@avaloq.com>
2024-03-18 14:06:05 +0530

    add terraform backend

--- provider.tf
+++ provider.tf
@@ -11,4 +11,15 @@ terraform {
       version = "5.18.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "avinash-test"
+    key            = "terraform.tfstate"
+    region         = "eu-zurich-1"
+    endpoint       = "https://objectstorage.eu-zurich-1.oraclecloud.com"
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+  }
+
 }
