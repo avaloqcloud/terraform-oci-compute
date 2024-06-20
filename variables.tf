ae58c4cb32ae4baa1d4aadeb8439a66aaa021977
Tejas Avhad <Tejas.Avhad@avaloq.com>
2024-06-20 14:02:32 +0530

    added nsg_ids

--- variables.tf
+++ variables.tf
@@ -88,5 +88,5 @@ variable "plugin_name" {
 variable "nsg_ids" {
   type = string
   description = "Network Security Group ID"
-  default = ""
+  default = "ocid1.networksecuritygroup.oc1.eu-zurich-1.aaaaaaaaidlhyglsbr6k4rcti6vvxrqaxg2svtxjkzwrfuhhwepwcmb6s7ba"
 }
